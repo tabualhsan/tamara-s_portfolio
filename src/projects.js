1479d84adf656434d9d1c22dc42ea384431e966c
Tamara <tabualhsan@gmail.com>
2021-07-30 11:44:16 -0700

    made some css fixes

--- src/projects.js
+++ src/projects.js
@@ -28,21 +28,20 @@ const Projects = () => {
 
     
         <motion.div variants={pageAnimation} initial="hidden" animate="show"  >
-        <div style={{display:"flex"}} className="card-deck" display="grid" className="mb-3">
-            <div >
-       
-        
+        <div className='card-deck' >
+            <div > 
             <CardDeck>
-            <Card  style={{ display:"grid", width: '20rem' }} >
+            <Card  style={{ width: '18rem' }} >
                 <Card.Body >
                 <Card.Title>Dow Jane</Card.Title>
                 <motion.div variants={lineAnime} className="line"></motion.div>
                 <a href="https://dowjane.app/" target="_blank" >
                 <motion.img variants={photoAnim}   style={{height:'15rem', objectFit:'cover'}} src={dowjane} alt={"dowjane logo"}/>
                 </a>
-                <Card.Text > I created Dow Jane with the intent to educate and empower women to be informed about the stock market and to be able to make better investment decisions.  Users sign up to a platform that eliminates the complexity of the stock market and provides real-time data about stocks. Utilizing chart.js and the Alpha Vantage API, Dow Jane populates monthly prices for over 10,000 stocks, and informs users if it is woman-lead. They can also favorite a stock to keep track of it for future investing. Finally, users can learn more of all women-led companies in the S&P 500, which only account for 6%.</Card.Text>
+                <Card.Text style={{padding:"20px"}}> Dow Jane is an educational platform to empower women to be informed about
+stock investments, and to make better informed investing decisions.</Card.Text>
             
-            <Card.Text>
+            <Card.Text style={{padding:"10px"}}>
                 <h6>Tech Stack:</h6>
                     <strong>Backend:</strong> Flask, Python3, SQLAlchemy, bycrypt, Alpha Vantage API, Flask <br></br>
                     <strong>Frontend:</strong> JavaScript, HTML5, CSS3, chart.js, jQuery, Ajax, Bootstrap<br></br>
@@ -60,20 +59,21 @@ const Projects = () => {
             </Card>
             </CardDeck>
         </div>
-            <div  style={{display:"grid"}}>
+            <div  >
             <CardDeck>
-            <Card style={{ width: '20rem', fontWeight:"small"}} className="mb-3">
+            <Card style={{ width: '18rem'}}>
                 <Card.Body >
                 <Card.Title>National Park Search App </Card.Title>
                 <motion.div variants={lineAnime} className="line"></motion.div>
                 <a href="https://nationalparkfinder.netlify.app/" target="_blank" >
                 <motion.img variants={photoAnim} style={{height:'15rem', objectFit:'cover'}} src={nationalpark} alt="National Park logo"/>
                 </a>
-                <Card.Text >The National Park Seach App for the adventerous individual out there looking to venture and discover the 432 National Parks that the USA has.</Card.Text>
-            <Card.Text><h6>Tech Stack:</h6>
+                <Card.Text style={{padding:"20px"}} >The National Park Seach App for the adventerous individual out there looking to venture and discover the 432 National Parks that the USA has.</Card.Text>
+            <Card.Text style={{padding:"10px"}}><h6>Tech Stack:</h6>
             <strong>Frontend:</strong> React, JavaScript, HTML5, CSS3,  Bootstrap <br></br>
             <strong>API:</strong> National Park Service API<br></br>
             <strong>Tools:</strong> GIT, GITHub, Visual StudioCode<br></br>
+            <strong>Website:</strong> <a href={"https://nationalparkfinder.netlify.app/"}>National Park Search</a>
             </Card.Text>
                 </Card.Body>
                 <Card.Link  style={{color:"#D96ED4" }} target="_blank" href={"https://github.com/tabualhsan/National_Park_Finder_App"}><AiFillGithub size={30}/>
@@ -82,15 +82,15 @@ const Projects = () => {
             </CardDeck>
             </div>
             
-            <div  style={{display:"grid",align:"center"}}>
+            <div>
             <CardDeck>
-            <Card style={{ width: '20rem', fontWeight:"small"}} className="mb-3">
+            <Card style={{ width: '18rem',}} >
                 <Card.Body >
                 <Card.Title style={{fontWeight:'medium'}}> Beats By Tamara </Card.Title>
                 <motion.div variants={lineAnime} className="line"></motion.div>
             <motion.img variants={photoAnim} style={{height:'15rem', objectFit:'cover'}} src={music2}/>
-                <Card.Text> Beats by Tamara was inspired to ignite you musical creativity with the ability to combine a range of beats to make your own!</Card.Text>
-            <Card.Text >
+                <Card.Text style={{padding:"20px"}}> Beats by Tamara was inspired to ignite you musical creativity with the ability to combine a range of beats to make your own!</Card.Text>
+            <Card.Text style={{padding:"10px"}} >
                 <h6>Tech Stack:</h6>
                 <strong>Frontend:</strong> JavaScript, HTML5, CSS3, Bootstrap<br></br>
                 <strong>Tools:</strong> GIT, GITHub, Visual StudioCode<br></br>
@@ -102,16 +102,16 @@ const Projects = () => {
             </Card>
             </CardDeck>
             </div>
-            <div  style={{display:"flex"}}>
+            <div>
             <CardDeck >
-            <Card style={{ width: '20rem', fontWeight:"small"}} className="mb-3">
+            <Card style={{ width: '18rem'}} >
                 <Card.Body >
                 <Card.Title style={{fontWeight:'medium'}}> Study Beats </Card.Title>
 
                 <motion.div variants={lineAnime} className="line"></motion.div>
                 <motion.img variants={photoAnim}style={{height:'15rem'}} src={Capture}/>
-                <Card.Text> A playlist of lofi beats to listen while you studying. </Card.Text>
-            <Card.Text >
+                <Card.Text style={{padding:"20px"}}> A playlist of lofi beats to listen while you studying. </Card.Text>
+            <Card.Text style={{padding:"10px"}} >
                 <h6>Tech Stack:</h6>
                 <strong>Frontend:</strong> React, JavaScript, HTML5, CSS3, Bootstrap<br></br>
                 <strong>Tools:</strong> GIT, GITHub, Visual StudioCode<br></br>
@@ -157,9 +157,13 @@ const Projects = () => {
 const Paragraph = styled.div`
     padding-bottom: 100rem;
     padding-top: 10rem;
-    display: grid;
-    grid-template-columns: auto auto auto;
-    padding:40px;
+    position:relative ;
+    width:100%;
+    height: 100%;
+ 
+
+
+    padding:10px;
         
     
     .line{
